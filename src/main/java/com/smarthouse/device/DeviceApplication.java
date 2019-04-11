bd7b95efdc6a5422c887c372503a2a2ce3d6f6e0
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-04-11 18:44:54 +0300

    Add EntityScan to device application
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- src/main/java/com/smarthouse/device/DeviceApplication.java
+++ src/main/java/com/smarthouse/device/DeviceApplication.java
@@ -2,11 +2,14 @@ package com.smarthouse.device;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
 
 @SpringBootApplication
+@EntityScan("com.smarthouse.commonutil.entities")
 public class DeviceApplication {
     public static void main(String[] args) {
-
         SpringApplication.run(DeviceApplication.class, args);
     }
 }
