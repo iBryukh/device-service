ad3a54d284e922c7bf8126cab9bda014d49343ba
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-04-11 18:45:11 +0300

    Create repository layer
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ src/main/java/com/smarthouse/device/repository/DeviceRepository.java
@@ -0,0 +1,9 @@
+package com.smarthouse.device.repository;
+
+import com.smarthouse.commonutil.entities.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+}
