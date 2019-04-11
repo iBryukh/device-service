eda98df8379f7acd4c70ea77f2b87c81935bd913
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-04-11 18:48:22 +0300

    Create device controller
    - Add get endpoint
    - Add post endpoint
    
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ src/main/java/com/smarthouse/device/controller/DeviceController.java
@@ -0,0 +1,41 @@
+package com.smarthouse.device.controller;
+
+import com.smarthouse.commonutil.data_transfer_objects.DeviceDTO;
+import com.smarthouse.commonutil.entities.Device;
+import com.smarthouse.commonutil.exceptions.ResourceNotFoundException;
+import com.smarthouse.device.service.DeviceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+import static com.smarthouse.commonutil.data_transfer_objects.DTOUtil.getDeviceFromDto;
+import static com.smarthouse.commonutil.exceptions.ResourceNotFoundException.getNoResourceMessage;
+
+@RestController
+public class DeviceController {
+
+    private DeviceService deviceService;
+
+    @Autowired
+    public DeviceController(DeviceService deviceService) {
+        this.deviceService = deviceService;
+    }
+
+    @GetMapping("/device/{id}")
+    public ResponseEntity<Device> getDeviceById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(deviceService.getById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(getNoResourceMessage("device", id))
+                ));
+    }
+
+    @PostMapping(name = "/device")
+    public ResponseEntity<Device> register(@RequestBody DeviceDTO deviceDTO) {
+        Objects.requireNonNull(deviceDTO);
+        Device device = getDeviceFromDto(deviceDTO);
+        return ResponseEntity.ok(deviceService.save(device));
+    }
+
+}
